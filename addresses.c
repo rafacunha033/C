9514be540b666236205803b8506ff503ca5e3f69
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-10-05 15:29:34 +0000

    ponter p

--- addresses.c
+++ addresses.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
+    // The variable "n" will be allocated at any place in memory using 4 bytes
     int n = 50;
-    printf("%i\n", n);
+    int *p = &n;
+    printf("%p\n", p);
 }
